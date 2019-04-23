63ff7a655b8539e6e98d7d0273e6c0c34b7f3bb7
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-04-23 08:53:09 +0300

    Добавьте файлы проекта.

---
+++ NET.S.2019.Pristavko.13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Pristavko._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
