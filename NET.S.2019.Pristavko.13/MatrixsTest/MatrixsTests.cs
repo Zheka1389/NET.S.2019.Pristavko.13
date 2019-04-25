eebc71db65a00fd9fe6c7e8ddde535cd99b306b1
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-04-25 20:44:24 +0300

    Add files via upload

---
+++ NET.S.2019.Pristavko.13/MatrixsTest/MatrixsTests.cs
@@ -0,0 +1,75 @@
+﻿namespace NET.S._2019.Pristavko._13.MatrixsTest
+{
+    using NET.S._2019.Pristavko._13.Matrixs;
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+
+    [TestFixture]
+    class MatrixsTests
+    {
+        [Test]
+        public void OperationAdd()
+        {
+            int[,] matrix = new int[,]
+            {
+                { 3, 5, 4},
+                { 5, 6, 8},
+                { 4, 8, 7}
+            };
+
+            Square<int> square = new Square<int>(matrix);
+            Symmetrical<int> symmetrical = new Symmetrical<int>(matrix);
+
+            int[] expected = new int[]
+            {
+                 6, 10, 8,
+                 10, 12, 16,
+                 8, 16, 14
+            };
+
+            var result = square.Add(symmetrical);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void OperationAddArgumentNullException()
+        {
+            int[,] param = new int[,]
+            {
+                { 3, 5, 4},
+                { 5, 6, 8},
+                { 4, 8, 7}
+            };
+
+            Square<int> square = new Square<int>(param);
+            Symmetrical<int> symmetrical = null;
+
+            Assert.Throws<ArgumentNullException>(
+                () => square.Add(symmetrical));
+        }
+
+        [Test]
+        public void OperationDifferenceSize()
+        {
+            int[,] param = new int[,]
+            {
+                { 3, 5, 4},
+                { 5, 6, 8},
+                { 4, 8, 7}
+            };
+
+            int[,] paramSquare = new int[,]
+            {
+                { 3, 5 },
+                { 5, 6 }
+            };
+
+            Square<int> square = new Square<int>(paramSquare);
+            Symmetrical<int> symmetrical = new Symmetrical<int>(param);
+
+            Assert.Throws<ArgumentException>(
+                () => square.Add(symmetrical));
+        }
+    }
+}
